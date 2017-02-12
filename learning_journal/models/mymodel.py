3911d7858c982494ccf5018e62b88481a1ed477c
Shu <shulatif@gmail.com>
2017-02-12 13:57:44 -0800

    Adding some work, almost done, doesn't work though.

--- learning_journal/models/mymodel.py
+++ learning_journal/models/mymodel.py
@@ -3,16 +3,45 @@ from sqlalchemy import (
     Index,
     Integer,
     Text,
+    DateTime,
+    String
 )
+from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from .meta import Base
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
+now = datetime.now()
+engine = create_engine('sqlite:///')
+db = sessionmaker(bind=engine)
+session = db()
+
+
+#class MyModel(Base):
+#    __tablename__ = 'models'
+#    id = Column(Integer, primary_key=True)
+#    name = Column(Text)
+#    value = Column(Integer)
+
+class Entry(Base):
+    __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+    title = Column(String(255), unique=True, nullable=False)
+    body = Column(Text)
+    created = Column(DateTime, default=now)
+    edited = Column(DateTime, default=now)
+
+    @classmethod
+    def all(cls):
+        return session.query(Entry).order_by(Entry.created)
+
+    @classmethod
+    def by_id(cls):
+        return session.query(Entry).order_by(Entry.id)
 
 
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+
+
+Index('entry', Entry.title, unique=True, mysql_length=255)
